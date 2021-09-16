f5d6d62b084b198ecc8c3e72109cbbe48e025ca8
ivan <ivanmvd@gmail.com>
2021-09-16 19:28:16 +0200

    comienzo de creación en el modelo

--- cliente/modelo.js
+++ cliente/modelo.js
@@ -12,3 +12,12 @@ function Juego(){
 function Jugador(nick){
     this.nick=nick;
 }
+
+function Partida(nombre){
+    this.nombre = nombre;
+}
+
+function Carta(color,tipo){
+    this.color = color;
+    this.tipo = tipo;
+}

693d4475452242dc7b94801442da473b43833f7a
ivan <ivanmvd@gmail.com>
2021-10-04 20:54:56 +0200

    Añado pruebas, creo clases principales y métodos para obtencion de cartas

--- cliente/modelo.js
+++ cliente/modelo.js
@@ -32,6 +32,14 @@ function Juego(){
             this.partidas[codigo].unirAPartida(jugador)
         }
     }
+    this.obtenerTodasPartidas=function(){
+        var lista = [];
+        for(each in this.partidas){
+            var partida = this.partidas[each];
+            lista.push({propietario:partida.propietario.partida.search})
+        }
+        return lista;
+    }
     this.obtenerCodigo=function(){
         let cadena="ABCDEFGHIJKLMNOPQRSTUVXYZ";
         let letras=cadena.split('');
@@ -56,12 +64,27 @@ function randomInt(low, high) {
 function Jugador(nick, juego){
     this.nick=nick;
     this.juego= juego;
+    this.mano=[];
+    this.codigoPartida;
     //El jugador es el que crea la partida
     this.crearPartida=function(numJug){
         return this.juego.crearPartida(nick,numJug);
     }
     this.unirAPartida=function(codigo){
         this.juego.unirAPartida(nick,codigo);
+    }
+    this.obtenerPartida=function(codigo){
+        return this.juego.partidas[codigo];
+    }
+    this.manoInicial=function(){
+        var partida = this.obtenerPartida(this.codigoPartida);
+        this.mano = partida.dameCartas(7);
+    }
+    this.robar=function(num){
+        var partida = this.obtenerPartida(this.codigoPartida);
+        var robadas = partida.dameCartas(num);
+        var tmp = this.mano ;
+        this.mano=tmp.concat(robadas);
     }
 }
 
@@ -70,6 +93,7 @@ function Partida(codigo,jugador,numJug){
     this.propietario =jugador.nick;
     this.numJug =numJug;
     this.jugadores={};
+    this.mazo = [];
 
     //Creamos la fase como Inicial
     this.fase = new Inicial();
@@ -79,6 +103,7 @@ function Partida(codigo,jugador,numJug){
     }
     this.puedeUnirAPartida = function(jugador){
         this.jugadores[jugador.nick] = jugador;
+        jugador.codigoPartida = this.codigo;
     }
     
 
@@ -87,8 +112,48 @@ function Partida(codigo,jugador,numJug){
         //Coge el nº de las claves del array asociativo (nick de Jugadores)  
         return Object.keys(this.jugadores).length;
     }
+    this.crearMazo=function(){
+        var colores=["rojo","azul","amarillo","verde"]
+
+        for (i=0;i <colores.length;i++) {
+            //Añadimos los 0
+            this.mazo.push(new Numero(0,colores[i]));  
+            //Añadimos el resto de números
+            for (j=1;j <10;j++) {
+                this.mazo.push(new Numero(j,colores[i]));         
+                this.mazo.push(new Numero(j,colores[i]));                    
+            }
+            for(j=0;j<2;j++){
+            //Añado los bloqueos, cambios  (2 por color)              
+            this.mazo.push(new Bloqueo("bloqueo",colores[i]));            
+            this.mazo.push(new Cambio("cambio",colores[i]));
+            this.mazo.push(new Mas2("mas2",colores[i]));
+            
+            }
+            //Añado el mas4, comodín y comodín4
+            this.mazo.push(new Mas4("mas4"));            
+            this.mazo.push(new Comodin("comodin"));
+            this.mazo.push(new Comodin4("comodin4"));
+        }    
+
+    }
+    this.asignarUnacarta=function(){
+        var longitudMazo = this.mazo.length;
+                //Obtienes la longitud del mazo
+        var aleatorio = Math.random() * (longitudMazo);
+        var cartaretirada = this.mazo.splice(aleatorio,1);
+        return cartaretirada[0];
+    }
+    this.dameCartas=function(num){
+        var cartas=[];
+        for (i = 0; i <num; i++) {
+            cartas.push(this.asignarUnacarta());            
+        }
+        return cartas;
+    }
 
         //Siempre tiene que ser la última línea
+    this.crearMazo();
     this.unirAPartida(jugador);
 }
 
@@ -118,7 +183,38 @@ function Final(){
     
 }
 
-function Carta(color,tipo){
+//Objetos del mazo
+//Números
+function Numero(valor,color){
     this.color = color;
-    this.tipo = tipo;
+    this.valor = valor;
+}
+//Bloqueos
+function Bloqueo(valor,color){
+    this.color = color;
+    this.valor = valor;
+}
+//Cambio
+function Cambio(valor,color){
+    this.color = color;
+    this.valor = valor;
+}
+//Mas2(Chupate)
+function Mas2(valor,color){
+    this.color = color;
+    this.valor = valor;
+}
+//Mas4(Chupate)
+function Mas4(valor){
+    this.valor = valor;    
+}
+//Comodín
+function Comodin(valor){
+    this.valor = valor;    
+}
+
+//Comodín4
+
+function Comodin4(valor){
+    this.valor = valor;    
 }
--- cliente/spec/ModeloSpec.js
+++ cliente/spec/ModeloSpec.js
@@ -7,47 +7,73 @@ describe("El juego del UNO...", function() {
     juego.agregarJugador("Juan");    
     juego.agregarJugador("Ana");    
     juego.agregarJugador("Pepe");
-
+  });
+  
+  it("Condiciones iniciales", function(){    
+    expect(juego.numeroPartidas()).toEqual(0); 
 
   });
-  //Bloque de pruebas
-  it("Ana crea una partida para 2 jugadores", function() {
 
-    var j1 = juego.usuarios["Ana"];    
-    expect(juego.numeroPartidas()).toEqual(0);
-    var partida = j1.crearPartida(2);
+  describe("Ana crea una partida de 1 jugadores..", function() {
+    var ju1;
+    var partida;
+  
+    beforeEach(function() {
+    //codigo para crear partida
+      ju1 = juego.usuarios["Ana"];   
+      partida = ju1.crearPartida(2); 
+    });
+
+  it("Comprobar mazo",function(){
+    expect(partida.mazo.length).toBe(112);
+        var rojo=partida.mazo.filter(function(each){
+          return each.color=="rojo";
+        });
+        expect(rojo.length).toBe(25);
+        var verde=partida.mazo.filter(function(each){
+          return each.color=="verde";
+        });
+        expect(verde.length).toBe(25);
+        var amarillo=partida.mazo.filter(function(each){
+          return each.color=="amarillo";
+        });
+        expect(amarillo.length).toBe(25);
+        var azul=partida.mazo.filter(function(each){
+          return each.color=="azul";
+        });
+        expect(azul.length).toBe(25);
+        var mas4=partida.mazo.filter(function(each){
+          return each.valor=="mas4";
+        });
+        expect(mas4.length).toBe(4);
+        var comodin=partida.mazo.filter(function(each){
+          return each.valor=="comodin";
+        });
+        expect(comodin.length).toBe(4);
+        var comodin4=partida.mazo.filter(function(each){
+          return each.valor=="comodin4";
+        });
+        expect(comodin4.length).toBe(4);
+  });
+
+  it("Comprobamos la partida para 2 jugadores", function() {
     expect(juego.numeroPartidas()).toEqual(1);    
     expect(partida.codigo).toBeDefined();
     expect(partida.numeroJugadores()).toEqual(1);    
     expect(partida.fase.nombre).toBe("Inicial");
   });
 
-  it("Ana crea una partida para 2 jugadores y Pepe se une",function(){
-      
-    var j1 = juego.usuarios["Ana"];   
-    var j2 = juego.usuarios["Pepe"];       
-    expect(juego.numeroPartidas()).toEqual(0); 
-
-    var partida = j1.crearPartida(2);
-    expect(partida.numeroJugadores()).toEqual(1);  
-    
-    j2.unirAPartida(partida.codigo);
-    
+  it(" Pepe se une",function(){
+    var j2 = juego.usuarios["Pepe"];           
+    j2.unirAPartida(partida.codigo);    
     expect(partida.numeroJugadores()).toEqual(2);
-
     expect(partida.fase.nombre).toBe("Jugando");
      
     
   });
-  it("Ana crea una partida para 2 jugadores, Pepe se une y Juan intenta unirse",function(){
-      
-    var j1 = juego.usuarios["Ana"];   
+  it(" Pepe se une y Juan intenta unirse",function(){
     var j2 = juego.usuarios["Pepe"];       
     var j3 = juego.usuarios["Juan"];       
-    expect(juego.numeroPartidas()).toEqual(0); 
-
-    var partida = j1.crearPartida(2);
-    expect(partida.numeroJugadores()).toEqual(1);  
     
     j2.unirAPartida(partida.codigo);    
     expect(partida.numeroJugadores()).toEqual(2);
@@ -60,4 +86,17 @@ describe("El juego del UNO...", function() {
     expect(partida.fase.nombre).toBe("Jugando");
 
   });
+  //Test para obtener partida y comprobar que el código partida esté definido
+  it(" El código de partida del jugador está definido", function(){
+    var codigo = ju1.codigoPartida
+    expect(ju1.obtenerPartida(codigo)).toBeDefined();
+  });
+  //Test para ver que la mano inicial tiene 7 cartas y que el mazo ha disminuído 7 cartas
+  it(" La mano inicial tiene 7 cartas", function(){
+    ju1.manoInicial();
+    expect(ju1.mano.length).toBe(7);
+  });
+
+
+});
 });
